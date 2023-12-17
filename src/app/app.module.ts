5500301bb103bb7b694cd87e05d7d7a8a1f286cd
Klaus Wiesel <klaus.wiesel@gmx.de>
2023-12-17 23:20:40 +0100

    removed api key from repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import {environment} from "./environments/environment";
 
 @NgModule({
   declarations: [
@@ -34,14 +35,7 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
     AppRoutingModule,
     BrowserAnimationsModule,
     ClarityModule,
-    provideFirebaseApp(() => initializeApp({
-      "projectId": "fire1-4849e",
-      "appId": "1:193706716968:web:8e2bbc603253980eaaaa1d",
-      "storageBucket": "fire1-4849e.appspot.com",
-      "apiKey": "AIzaSyC64bby7tQrjkkiJ2z_Db_kXXgEeCzTzEU",
-      "authDomain": "fire1-4849e.firebaseapp.com",
-      "messagingSenderId": "193706716968"
-    })),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     ReactiveFormsModule,
