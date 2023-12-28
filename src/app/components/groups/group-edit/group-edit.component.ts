dec6cb2fd12df8dabbbc06a8ef0b9b0e0e6e4a22
Klaus Wiesel <klaus.wiesel@gmx.de>
2023-12-28 15:21:06 +0100

    combo grades

--- src/app/components/groups/group-edit/group-edit.component.ts
+++ src/app/components/groups/group-edit/group-edit.component.ts
@@ -1,9 +1,6 @@
 import {Component, EventEmitter, inject, Output} from '@angular/core';
 import {Group} from "../../../models/group";
 import {FirebaseService} from "../../../services/firebase.service";
-import {Observable, of} from "rxjs";
-import {Grade} from "../../../models/grade";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {StateService} from "../../../services/state.service";
 
 @Component({
@@ -15,20 +12,12 @@ export class GroupEditComponent {
   show = false;
   fbs = inject(FirebaseService);
   stateService = inject(StateService);
-  grades$: Observable<Grade[]> = of([]);
   item!: Group;
 
   @Output() onApply: EventEmitter<any> = new EventEmitter<any>();
   @Output() onCancel: EventEmitter<any> = new EventEmitter<any>();
-  grades!: Array<Grade>;
-  levels: number[];
-
-  constructor() {
-    this.levels = this.stateService.grades.map(item => item.level);
-  }
 
   open(item: Partial<Group>) {
-    console.log('open', item, this.levels)
     this.item = Object.create(item);
     this.show = true;
   }
