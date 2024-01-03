7b07ca1b345f8325fe629f8abc1aa7d501039e7f
Klaus Wiesel <klaus.wiesel@gmx.de>
2024-01-03 16:50:54 +0100

    course, grade edit as form

--- src/app/components/courses/course-edit/course-edit.component.ts
+++ src/app/components/courses/course-edit/course-edit.component.ts
@@ -1,57 +1,80 @@
-import {Component, EventEmitter, inject, Output} from '@angular/core';
+import {Component, EventEmitter, inject, OnInit, Output} from '@angular/core';
 import {Course} from "../../../models/course";
 import {CoursePerWeek} from "../../../models/coursePerWeek";
 import {StateService} from "../../../services/state.service";
 import { Guid } from 'guid-typescript';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormHelperService} from "../../../services/form-helper.service";
+import _ from "lodash";
 
 @Component({
   selector: 'app-course-edit',
   templateUrl: './course-edit.component.html',
   styleUrl: './course-edit.component.css'
 })
-export class CourseEditComponent {
-  show = false;
-  item!: Course;
+export class CourseEditComponent implements OnInit {
   stateService = inject(StateService);
-  // @ts-ignore
-  form: FormGroup = null;
+  formBuilder = inject(FormBuilder);
+  formHelper = inject(FormHelperService);
+
+  show = false;
+  form!: FormGroup;
+  item!: Partial<Course>;
 
   @Output() onApply: EventEmitter<any> = new EventEmitter<any>();
   @Output() onCancel: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor( ) {
-    this.form = new FormGroup({
-      name: new FormControl('', Validators.required),
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: this.formBuilder.control(undefined),
     })
+    this.formHelper.addDefaultControls(this.form, this.formBuilder);
   }
 
   open(item: Partial<Course>) {
-    this.item = Object.create(item);
+    this.item = _.cloneDeep(item);
+    this.form.patchValue(_.cloneDeep(item));
     this.item.deleteHours = [];
     const list = this.stateService.coursesPerWeek.filter(courseweek => item.id === courseweek.courseId) ?? []
     this.item.weeklyHours = list.sort((a,b) => a.level - b.level);
+    if (!this.item.weeklyHours) {
+      this.item.weeklyHours = [];
+    }
     this.show = true;
-    console.log('openitem', item, list)
   }
 
   addHour() {
     const newItem: Partial<CoursePerWeek> = {courseId: this.item.id, hours: 1, isNew: true, id: Guid.create().toString()}
-    this.item.weeklyHours.push(newItem);
+    this.item.weeklyHours?.push(newItem);
   }
 
   removeHour(item: Partial<CoursePerWeek>): void {
-    const index = this.item.weeklyHours.findIndex(i => (i.id == item.id));
+    const index = this.item.weeklyHours?.findIndex(i => (i.id == item.id)) ?? -1;
     if (index !== -1) {
-      this.item.deleteHours.push(<string>item.id);
-      this.item.weeklyHours.splice(index,1);
+      this.item.deleteHours?.push(<string>item.id);
+      this.item.weeklyHours?.splice(index, 1);
     }
   }
 
   apply() {
-    if (this.item.name.length === 0) {
+    const item = this.form.getRawValue();
+    if (item.name.length === 0) {
       return;
     }
-    this.onApply.emit(this.item);
+    item.deleteHours = this.item.deleteHours;
+    item.weeklyHours = this.item.weeklyHours;
+    this.onApply.emit(item);
+  }
+
+  grades(courseWeek: Partial<CoursePerWeek>) {
+    const grades = this.stateService.grades;
+    this.item.weeklyHours?.forEach(h => {
+      const index = grades.findIndex(g => g.level == h.level);
+      if (index !== -1) {
+        grades.splice(index, 1);
+      }
+    });
+
+    return grades;
   }
 }
